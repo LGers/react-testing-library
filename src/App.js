090198d65fb51f73fb4f732d3027eb12233af3df
LGers <87089817+LGers@users.noreply.github.com>
2022-03-27 10:54:45 +0300

    lesson-1 -3 upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
+import React, { useState } from "react"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Search = ({value, onChange, children}) => (
+  <div>
+    <label htmlFor='search'>{children}</label>
+    <input id="search" type="text" value={value} onChange={onchange} placeholder='search text...'></input>
+  </div>
+)
+
+const App = () => {
+
+  const [search, setSearch] = useState("")
+
+  const handleChange = ({ target }) => {
+    setSearch(target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src='' alt='search image'/>
+      <Search value={search} onChange={handleChange}>
+        Search:
+      </Search>
+      <p>Searches for {search ? search : "..."}</p>
     </div>
-  );
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React testing library
+    //     </a>
+    //   </header>
+    // </div>
+  )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,43 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+//lesson-1
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+  screen.debug();
+});
+
+//lesson-2
+//snapshot
+test('renders learn react link2', () => {
+  const { asFragment } = render(<App />);
+  expect(asFragment(<App />)).toMatchSnapshot();
+});
+
+// test('renders Search', () => {
+//   const { asFragment } = render(<Search />);
+//   expect(asFragment(<Search />)).toMatchSnapshot();
+// });
+
+//lesson-3
+describe("App", () => {
+  it("render App component", () => {
+    render(<App />);
+    screen.debug();
+    expect(screen.getByText(/Search:/i)).toBeInTheDocument();
+  });
+});
+
+describe("App2", () => {
+  it("render App component", () => {
+    render(<App />);
+    expect(screen.getByText(/Search:/i)).toBeInTheDocument();
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('search text...')).toBeInTheDocument();
+    expect(screen.getByAltText('search image')).toBeInTheDocument();
+    expect(screen.getByDisplayValue('')).toBeInTheDocument();
+  });
 });
