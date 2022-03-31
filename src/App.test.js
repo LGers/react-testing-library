557ca76913164c6729b5cb97e4118e8b6ed3c2dd
LGers <87089817+LGers@users.noreply.github.com>
2022-03-31 09:18:27 +0300

    lesson_06 fireEvent, userEvent,  focus element select option

--- src/App.test.js
+++ src/App.test.js
@@ -45,4 +45,38 @@ describe("events", () => {
     expect(handleChange).toHaveBeenCalledTimes(1);
     expect(checkbox).toBeChecked();
   });
+
+  it("tab focus element", () => {
+    const { getAllByTestId } = render(
+      <div>
+        <input data-testid="element" type="checkbox" />
+        <input data-testid="element" type="radio" />
+        <input data-testid="element" type="number" />
+      </div>
+    );
+    const [checkbox, radio, number] = getAllByTestId('element');
+    userEvent.tab();
+    expect(checkbox).toHaveFocus();
+    userEvent.tab();
+    expect(radio).toHaveFocus();
+    userEvent.tab();
+    expect(number).toHaveFocus();
+  });
+
+  it("select option", () => {
+    const { selectOptions, getByRole, getByText } = render(
+      <select>
+        <option value="1">A</option>
+        <option value="2">B</option>
+        <option value="3">C</option>
+      </select>
+    );
+
+    userEvent.selectOptions(getByRole("combobox"), "1");
+    expect(getByText("A").selected).toBeTruthy();
+    
+    userEvent.selectOptions(getByRole("combobox"), "2");
+    expect(getByText("B").selected).toBeTruthy();
+    expect(getByText("A").selected).toBeFalsy();
+  });
 });
